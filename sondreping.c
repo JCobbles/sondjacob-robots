d9772ed2537bf87e87eb23e9f9e337a6ef1106ce
Sondre Agledahl <sondre.milch@gmail.com>
2017-02-28 17:23:39 +0000

    new sondreping version.

--- sondreping.c
+++ sondreping.c
@@ -6,6 +6,24 @@
 
 #define MAX_SPEED 128 // in ticks / sec
 
+typedef struct Node {
+    int rotation;
+    int forward_movement;
+    struct Node* next;
+} Node;
+
+Node* addToTop(int rot, int fwd, Node* current_node) {
+    Node* newNode = malloc(sizeof(Node));
+    newNode->rotation = rot;
+    newNode->forward_movement = fwd;
+    newNode->next = current_node;
+}
+
+Node* pop(Node* current_node)
+{
+    Node* node_to_pop = current_node;
+}
+
 double radius = 52.9;
 
 int newRound(double x)
@@ -25,8 +43,23 @@ int turn(int degrees)
   drive_goto(ticks, -ticks);
 }
 
+Node* turnAndStore(int degrees, Node* current_node) {
+    Node* new_node = addToTop(degrees, 20, current_node);
+    turn(degrees);
+    return new_node;
+}
+
 void forwards(int distance) {
     drive_goto(calculateTicks(distance), calculateTicks(distance));
+}
+
+void printList(Node* top_node) {
+    int number = 1;
+    while (top_node != NULL) {
+        printf("Node #%d: Rotation: %d, Forward movement: %d.\n", number++,
+        top_node->rotation, top_node->forward_movement);
+        top_node = top_node->next;
+    }
 }
 
 int irLeft, irRight;
@@ -43,6 +76,11 @@ int main() {
   low(26);  
   low(27);  
 
+  Node* current_node = malloc(sizeof(Node));
+  current_node->rotation = 0;
+  current_node->forward_movement = 0;
+  current_node->next = NULL;
+  
   while(1) {
     irLeft = 0;                                     
     irRight = 0;                                    
@@ -62,8 +100,9 @@ int main() {
 
     double Pout = calculatePout(irLeft, irRight);
 
-    turn(Pout);
+    current_node = turnAndStore(Pout, current_node);
     forwards(20);
+    
     distance = ping_cm(8);
     if (distance <= 10) {
         high(26);
@@ -71,7 +110,15 @@ int main() {
         low(26);
         break;
     }
-    
-    pause(10);
+    printList(current_node);   
   }
+  
+    turn(178);
+  
+    while (current_node != NULL) {
+        forwards(20);
+        turn(current_node->rotation * -1);
+        current_node = current_node->next;
+        printList(current_node);
+    }
 }
