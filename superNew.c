cbfcbb5b84fdb0cdf6542166d56e96838d9fd7a5
Sondre Agledahl <sondre.milch@gmail.com>
2017-03-10 15:16:32 +0000

    somethingsomething

--- superNew.c
+++ superNew.c
@@ -5,6 +5,7 @@
 #define true 1
 #define false 0
 
+#define MAX_SPEED 128 // in ticks / sec
 #define FWD 20
 
 typedef struct Node {
@@ -50,7 +51,7 @@ Node* turn(int degrees, Node* current_node) {
 
 int getTurnAmount(int degrees) {
     double radians = degrees *  PI / 180;
-    return newAbs(calculateTicks(radians * radius));
+    return calculateTicks(radians * radius);
 }
 
 void turnWithoutStore(int degrees) {
@@ -64,6 +65,19 @@ Node* forwards(int distance, Node* current_node) {
     return new_node;
 }
 
+void printStack(Node* top_node) {
+    int number = 1;
+    while (top_node != NULL) {
+        printf("Node #%d: Left %d, Right: %d.\n", number++,
+        top_node->left, top_node->right);
+        top_node = top_node->next;
+    }
+}
+
+void printNode(Node* node) {
+    printf("Storing... Left: %d, Right: %d.\n", node->left, node->right);
+}
+
 void freeStack(Node* top_node) {
     Node* temp;
     while (top_node != NULL) {
@@ -74,6 +88,7 @@ void freeStack(Node* top_node) {
 }
 
 int irLeft, irRight;
+double epsilon = 0.001f;
 double Kp = 1;
 
 double calculatePout(int left, int right) {
@@ -81,6 +96,7 @@ double calculatePout(int left, int right) {
 }
 
 int main() {
+  // drive_speed(MAX_SPEED, MAX_SPEED);
   int distance, irLeft, irRight;
 
   low(26);  
@@ -93,10 +109,9 @@ int main() {
   
   Node* current_node = top_node;
   int temp_sum = 0;
-  int turned; // undercover boolean
+  int turned_last = false; // undercover boolean
   
   while(true) {
-    turned = false;
     irLeft = 0;                                     
     irRight = 0;                                    
 
@@ -109,26 +124,34 @@ int main() {
       freqout(1, 1, 38000);
       irRight += input(2);                  
     }                                               
-
     double Pout = calculatePout(irLeft, irRight);
-    if (getTurnAmount(Pout) > 0) {
-        current_node = turn(Pout, current_node);
-        turned = true;
-    }    
+    if (newAbs(getTurnAmount(Pout)) > 1) {
+        turnWithoutStore(Pout);
+        if (turned_last) {
+            current_node->left += getTurnAmount(Pout);
+            current_node->right -= getTurnAmount(Pout);
+            printNode(current_node);
+        } else {
+            current_node = push((temp_sum / 2) + getTurnAmount(Pout), (temp_sum / 2) - getTurnAmount(Pout), current_node);
+            printNode(current_node);
+            current_node = push(temp_sum / 2, temp_sum / 2, current_node);
+            printNode(current_node);
+            temp_sum = 0;
+        }
+        printNode(current_node);
+        turned_last = true;
+    } else {
+        turned_last = false;
+    }
     
     temp_sum += calculateTicks(FWD);
+    printf("Incrementing temp_sum... %d\n", temp_sum);
     drive_goto(calculateTicks(FWD), calculateTicks(FWD));
-    
-    if (turned) {
-        turned = false;
-        current_node = push(temp_sum, temp_sum, current_node);
-        temp_sum = 0;
-    }
  
     distance = ping_cm(8);
     if (distance <= 10) {
         if (temp_sum > 0) {
-            current_node = push(calculateTicks(temp_sum), calculateTicks(temp_sum), current_node);
+            current_node = push(temp_sum, temp_sum, current_node);
         } 
         high(26);
         pause(100);
@@ -139,10 +162,21 @@ int main() {
     top_node = current_node;
     
     turnWithoutStore(179);
+    Node temp_node;
+   
     
     while (current_node->next != NULL) {
-        drive_goto(current_node->right, current_node->left);
-        current_node = current_node->next;
+        temp_node.left = 0;
+        temp_node.right = 0;
+        for (int i = 0; i < 4; i++) {
+            if (current_node == NULL) break;
+            temp_node.left += current_node->left;
+            temp_node.right += current_node->right;
+            current_node = current_node->next;
+        }
+        printf("Temp_node, left: %d, %d\n", temp_node.left, temp_node.right);
+        drive_goto(temp_node.right, temp_node.left);
+        if (current_node != NULL) printStack(current_node);
     }
     freeStack(top_node);
 }
