b18c1a84262da52f98207ba9d182e9fdc0a1791a
Sondre Agledahl <sondre.milch@gmail.com>
2017-03-17 16:19:09 +0000

    naviagtion in MAZE.c

--- MAZE.c
+++ MAZE.c
@@ -9,7 +9,13 @@
 #define NEVER 0
 #define ONCE 1
 #define TWICE 2
-#define PATH 3
+#define CORRIDOR 3
+
+#define NO_UNVISITED_PATH 0
+#define NORTH 1
+#define WEST 2
+#define EAST 3
+#define SOUTH 4
 
 #define MAX_SPEED 128 // in ticks / sec
 #define FWD 40
@@ -50,14 +56,18 @@ Square* current_pos;
 
 int main() {
     int distance, irLeft, irRight;
+    int direction_to_move;
     low(26);  
     low(27);
     forwards(FWD);
-    current_pos = malloc(sizeof(Square));
+    current_pos = calloc(1, sizeof(Square));
     current_pos->x = 0;
     current_pos->y = 0;
+    current_pos->visited = NEVER;
         
     while (true) {
+        direction_to_move = NO_UNVISITED_PATH;
+        
         for(int dacVal = 0; dacVal < 160; dacVal += 8) {                                               
             dac_ctr(26, 0, dacVal);   
             freqout(11, 1, 38000);      
@@ -68,10 +78,80 @@ int main() {
             irRight += input(2);                  
         }
         
+        if (irLeft < 20) {
+            // TODO: convert this to a global position
+            // mark that there is a wall to the left
+            current_pos->west = NULL;
+        } else if (current_pos->west == NULL) {
+            // if square to left has not been seen, allocate a position for it in memory
+            current_pos->west = calloc(1, sizeof(Square));
+            current_pos->west->x = current_pos->x - 1;
+            current_pos->west->y = current_pos->y;
+            current_pos->west->visited = NEVER;
+            direction_to_move = WEST;
+        }
+        else if (current_pos->west->visited == NEVER) {
+            direction_to_move = WEST;
+        }
         
+        if (irRight < 20) {
+            // TODO: convert this to a global position
+            current_pos->east = NULL;
+        } else if (current_pos->east == NULL) {
+            current_pos->east = calloc(1, sizeof(Square));
+            current_pos->east->x = current_pos->x + 1;
+            current_pos->east->y = current_pos->y;
+            current_pos->east->visited = NEVER;
+            if (direction_to_move == NO_UNVISITED_PATH) {
+                direction_to_move = EAST;
+            }
+        } else if (current_pos->east->visited == NEVER && direction_to_move == NO_UNVISITED_PATH) {
+            direction_to_move = EAST;
+        }
         
+        distance = ping_cm(8);
+        if (distance < 20) {
+            // TODO: convert this to a global position
+            current_pos->north = NULL;
+        } else if (current_pos->north == NULL) {
+            current_pos->north->x = current_pos->x;
+            current_pos->north->y = current_pos->y + 1;
+            current_pos->north->visited = NEVER;
+            if (direction_to_move == NO_UNVISITED_PATH) {
+                direction_to_move = NORTH;
+            }
+        } else if (current_pos->north->visited == NEVER && direction_to_move == NO_UNVISITED_PATH) {
+            direction_to_move = NORTH;
+        }
+        
+        switch (direction_to_move) {
+            Square* temp = current_pos;
+            case NORTH:
+                forwards(FWD);
+                current_pos = current_pos->north;
+                current_pos->south = temp;
+                current_pos->visited++;
+                break;
+            case WEST:
+                turn(90);
+                forwards(FWD);
+                current_pos = current_pos->west;
+                current_pos->east = temp;
+                current_pos->visited++;
+                break;
+            case EAST:
+                turn(-90);
+                forwards(FWD);
+                current_pos = current_pos->east;
+                current_pos->west = temp;
+                current_pos->visited++;
+                break;
+            case NO_UNVISITED_PATH:
+                turn(180);
+                forwards(FWD);
+                break;
+        }
     }
-    
-    
+    printf("Iteration complete\n");
 }
 
