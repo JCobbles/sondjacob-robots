71ffa4f4b970b088d10a6e1a1efab0ed3f867157
colonelsalt <sondre.milch@gmail.com>
2017-03-21 14:47:08 +0000

    Update newVersion.c

--- newVersion.c
+++ newVersion.c
@@ -1,148 +1,412 @@
 
 #include "simpletools.h"
 #include "ping.h"
+#include "abdrive.h"
+#include <stdio.h>
 
 #define true 1
 #define false 0
 
-#define FWD 20
+#define UNKNOWN 0
+#define NEVER 1
+#define ONCE 2
+#define TWICE 3
+#define CORRIDOR 4
 
-typedef struct Node {
-    int left;
-    int right;
-    struct Node* next;
-} Node;
+#define NORTH 0
+#define WEST 1
+#define EAST 2
+#define SOUTH 3
 
-Node* push(int ticksLeft, int ticksRight, Node* current_node) {
-    Node* newNode = malloc(sizeof(Node));
-    newNode->left = ticksLeft;
-    newNode->right = ticksRight;
-    newNode->next = current_node;
-    return newNode;
-}
+#define MAX_SPEED 128 // in ticks / sec
+#define FWD 420
+
+int currentDirection;
+
+typedef struct Square {
+    int x;
+    int y;
+    int visited;
+    int is_junction;
+    struct Square* north;
+    struct Square* west;
+    struct Square* east;
+    struct Square* south;
+} Square;
+
 double radius = 52.9;
 
-Node* drive_store(int left, int right, Node* current_node) {
-    Node* new_node = push(left, right, current_node);
-    drive_goto(left, right);
-    return new_node;
-}
-
 int newRound(double x) {
-  if (x - (int) x < 0.5) return (int) x;
-  else return (int) x + 1;
-}
-
-double newAbs(double x) {
-    return (x >= 0) ? x : -x;
+    if (x - (int) x < 0.5) return (int) x;
+    else return (int) x + 1;
 }
 
 int calculateTicks(int distanceInMillimetres) {
-  return newRound(distanceInMillimetres / 3.25);
+    return newRound(distanceInMillimetres / 3.25);
 }
 
-Node* turn(int degrees, Node* current_node) {
-  double radians = degrees *  PI / 180.0;
-  int ticks = calculateTicks(radians * radius);
-  Node* new_node = drive_store(ticks, -ticks, current_node);
-  return new_node;
-}
-
-int getTurnAmount(int degrees) {
-    double radians = degrees *  PI / 180;
-    return newAbs(calculateTicks(radians * radius));
-}
-
-void turnWithoutStore(int degrees) {
-    double radians = degrees *  PI / 180;
-    int ticks = calculateTicks(radians * radius);
-    drive_goto(ticks, -ticks);
-}
-
-Node* forwards(int distance, Node* current_node) {
-    Node* new_node = drive_store(calculateTicks(distance), calculateTicks(distance), current_node);
-    return new_node;
-}
-
-void freeStack(Node* top_node) {
-    Node* temp;
-    while (top_node != NULL) {
-        temp = top_node->next;
-        free(top_node);
-        top_node = temp;
+void turn(int degrees) {
+    if (degrees == 90) {
+        drive_goto(26, -25);
+    } else if (degrees == -90) {
+        drive_goto(-25, 26);
+    } else if (degrees == 180) {
+        drive_goto(52, -50);
+    } else {
+        double radians = degrees *  PI / 180;
+        int ticks = calculateTicks(radians * radius);
+        drive_goto(ticks, -ticks);
     }
 }
 
-int irLeft, irRight;
-double Kp = 1;
+void forwards(int distance) {
+    drive_goto(calculateTicks(distance), calculateTicks(distance));
+}
 
-double calculatePout(int left, int right) {
-  return Kp * (right - left);
+Square* current_pos;
+Square* start_pos;
+
+int reverseDirection(int direction) {
+    return (direction - 2) % 4;
+}
+
+int calculateCardinalDirection(int localDirection) {
+    if (currentDirection == NORTH) return localDirection;
+    else if (localDirection == NORTH) return currentDirection;
+    
+    else if (currentDirection == WEST) {
+        switch (localDirection) {
+            case WEST: return SOUTH;
+            case EAST: return NORTH;
+            case SOUTH: return EAST;
+        }
+    } else if (currentDirection == EAST) {
+        switch (localDirection) {
+            case WEST: return NORTH;
+            case EAST: return SOUTH;
+            case SOUTH: return WEST;
+        }
+    } else if (currentDirection == SOUTH) {
+        switch (localDirection) {
+            case WEST: return EAST;
+            case EAST: return WEST;
+            case SOUTH: return NORTH;
+        }
+    }
+}
+
+int isRightDirection(Square* square) {
+    return square->visited == ONCE || square->visited == CORRIDOR;
+}
+
+void move(int direction) {
+    forwards(40);
+    currentDirection = direction;
+    switch (direction) {
+        case NORTH:
+            break;
+        case EAST:
+            break;
+        case SOUTH:
+            break;
+        case WEST:
+            break;
+    }
+}
+
+void returnJourney() {
+    while (true) {
+        if (isRightDirection(current_pos->south)) {
+            current_pos = current_pos->south;
+            move(SOUTH);
+        } else if (isRightDirection(current_pos->west)) {
+            current_pos = current_pos->west;
+            move(WEST);
+        } else if (isRightDirection(current_pos->north)) {
+            current_pos = current_pos->north;
+            move(NORTH);
+        } else if (isRightDirection(current_pos->east)) {
+            current_pos = current_pos->east;
+            move(EAST);
+        }
+    }
+}
+
+int direction_to_move, num_free_paths;
+
+Square* initGrid() {
+    Square* origin = malloc(sizeof(Square));
+    Square* square = origin;
+    Square* grid[4][4];
+    grid[0][0] = origin;
+    for (int i = 0; i < 4; i++) {
+        Square* leftmost = square;
+        for (int j = 0; j < 4; j++) {
+            square->x = j;
+            square->y = i;
+            square->visited = UNKNOWN;
+            square->is_junction = false;
+            grid[j][i] = square;
+            if (i != 0) {
+                square->south = grid[j][i - 1];
+                grid[j][i - 1]->north = square;
+            }
+            if (j != 3) {
+                Square* temp = square;
+                square->east = malloc(sizeof(Square));
+                square = square->east;
+                square->west = temp;
+            }
+        }
+        leftmost->north = malloc(sizeof(Square));
+        square = leftmost->north;
+    }
+    return origin;
+}
+
+int lowest_visited_option, num_free_paths;
+Square* prev_square;
+
+void analyseSquare(Square* current_pos, int localDirection, int wall_distance) {
+    if (wall_distance < 10) {
+        forwards(-30); // back off a little if wall is too close
+    }
+    int globalDirection = calculateCardinalDirection(localDirection);
+    switch (globalDirection) {
+        case NORTH:
+            if (wall_distance < 40 || current_pos->north == NULL) {
+                current_pos->north = NULL;
+                printf("Detected wall to the north\n");
+                return;
+            }
+            else if (current_pos->north->visited == UNKNOWN || current_pos->north->visited == NEVER) {
+                current_pos->north->visited = NEVER;
+                lowest_visited_option = NEVER;
+                direction_to_move = localDirection;
+                printf("Found unvisited square to the north\n");
+            }
+            else {
+                if (current_pos->visited == NEVER && current_pos->south->visited <= lowest_visited_option) {
+                    lowest_visited_option = current_pos->south->visited;
+                    direction_to_move = localDirection;
+                }
+                else if (current_pos->north->visited < lowest_visited_option) {
+                    lowest_visited_option = current_pos->north->visited;
+                    direction_to_move = localDirection;
+                }
+                printf("Found previously visited square to the north\n");
+            }
+            num_free_paths++;    
+            return;
+        case WEST:
+            if (wall_distance < 40 || current_pos->west == NULL) {
+                current_pos->west = NULL;
+                printf("Detected wall to the west\n");
+                return;
+            }
+            else if (current_pos->west->visited == UNKNOWN || current_pos->west->visited == NEVER) {
+                current_pos->west->visited = NEVER;
+                lowest_visited_option = NEVER;
+                direction_to_move = localDirection;
+                printf("Found unvisited square to the west\n");
+            }
+            else {
+                if (current_pos->visited == NEVER && current_pos->south->visited <= lowest_visited_option) {
+                    lowest_visited_option = current_pos->south->visited;
+                    direction_to_move = localDirection;
+                }
+                else if (current_pos->west->visited < lowest_visited_option) {
+                    lowest_visited_option = current_pos->west->visited;
+                    direction_to_move = localDirection;
+                }
+                printf("Found previously visited square to the west\n");
+            }
+            num_free_paths++;    
+            return;
+        case EAST:
+            if (wall_distance < 40 || current_pos->east == NULL) {
+                current_pos->east = NULL;
+                printf("Detected wall to the east\n");
+                return;
+            }
+            else if (current_pos->east->visited == UNKNOWN || current_pos->east->visited == NEVER) {
+                current_pos->east->visited = NEVER;
+                lowest_visited_option = NEVER;
+                direction_to_move = localDirection;
+                printf("Found unvisited square to the east\n");
+            }
+            else {
+                if (current_pos->visited == NEVER && current_pos->south->visited <= lowest_visited_option) {
+                    lowest_visited_option = current_pos->south->visited;
+                    direction_to_move = localDirection;
+                }
+                else if (current_pos->east->visited < lowest_visited_option) {
+                    lowest_visited_option = current_pos->east->visited;
+                    direction_to_move = localDirection;
+                }
+                printf("Found previously visited square to the east\n");
+            }
+            num_free_paths++;    
+            return;
+        case SOUTH:
+            if (wall_distance < 40 || current_pos->south == NULL) {
+                current_pos->south = NULL;
+                printf("Detected wall to the south\n");
+                return;
+            }
+            else if (current_pos->south->visited == UNKNOWN || current_pos->south->visited == NEVER) {
+                current_pos->south->visited = NEVER;
+                lowest_visited_option = NEVER;
+                direction_to_move = localDirection;
+                printf("Found unvisited square to the south\n");
+            }
+            else {
+                if (current_pos->visited == NEVER && current_pos->south->visited <= lowest_visited_option) {
+                    lowest_visited_option = current_pos->south->visited;
+                    direction_to_move = localDirection;
+                }
+                else if (current_pos->south->visited < lowest_visited_option) {
+                    lowest_visited_option = current_pos->south->visited;
+                    direction_to_move = localDirection;
+                }
+                printf("Found previously visited square to the south\n");
+            }
+            num_free_paths++;    
+            return;
+    }
+}
+
+int continuePath(Square* current_pos) {
+    int way_back = calculateCardinalDirection(SOUTH);
+    for (int i = NORTH; i < SOUTH; i++) {
+        int globalDirection = calculateCardinalDirection(i);
+        switch (globalDirection) {
+            case NORTH:
+                if (current_pos->north != NULL && way_back != NORTH) return i;
+                break;
+            case WEST:
+                if (current_pos->west != NULL && way_back != WEST) return i;
+                break;
+            case EAST:
+                if (current_pos->east != NULL && way_back != EAST) return i;
+                break;
+            case SOUTH:
+                if (current_pos->south != NULL && way_back != SOUTH) return i;
+                break;
+        }
+    }
+    return SOUTH;
 }
 
 int main() {
-  int distance, irLeft, irRight;
-
-  low(26);  
-  low(27);
-
-  Node* top_node = malloc(sizeof(Node));
-  top_node->left = 0;
-  top_node->right = 0;
-  top_node->next = NULL;
-  
-  Node* current_node = top_node;
-  int temp_sum = 0;
-  int turned; // undercover boolean
-  
-  while(true) {
-    turned = false;
-    irLeft = 0;                                     
-    irRight = 0;                                    
-
-    for(int dacVal = 0; dacVal < 160; dacVal += 8) {                                               
-      dac_ctr(26, 0, dacVal);   
-      freqout(11, 1, 38000);      
-      irLeft += input(10); 
-
-      dac_ctr(27, 1, dacVal);
-      freqout(1, 1, 38000);
-      irRight += input(2);                  
-    }                                               
-
-    double Pout = calculatePout(irLeft, irRight);
-    if (getTurnAmount(Pout) > 0) {
-        current_node = turn(Pout, current_node);
-        turned = true;
-    }    
-    
-    temp_sum += calculateTicks(FWD);
-    drive_goto(calculateTicks(FWD), calculateTicks(FWD));
-    
-    if (turned) {
+    low(26);  
+    low(27);
+    forwards(FWD);
+    start_pos = initGrid();
+    start_pos->visited = NEVER;
+    start_pos->south = NULL;
+    current_pos = start_pos;
+    currentDirection = NORTH;
+    int temp_direction;
+    int turned;
+    int turn_amount;
+        
+    while (true) {
+        printf("\nEntered square (%d, %d)\n", current_pos->x, current_pos->y);        
+        direction_to_move = SOUTH;
         turned = false;
-        current_node = push(temp_sum, temp_sum, current_node);
-        temp_sum = 0;
-    }
- 
-    distance = ping_cm(8);
-    if (distance <= 10) {
-        if (temp_sum > 0) {
-            current_node = push(calculateTicks(temp_sum), calculateTicks(temp_sum), current_node);
-        } 
-        high(26);
+        num_free_paths = 0;
         pause(100);
-        low(26);
-        break;
+        if (current_pos->x == 3 && current_pos->y == 3) {
+            high(26);
+            printf("Reached the end of the maze!\n");
+            pause(200);
+            low(26);
+            break;
+        }
+        
+        if (current_pos->visited < ONCE) {
+            turned = true;
+        
+            analyseSquare(current_pos, NORTH, ping_cm(8));
+            turn(-90);
+            pause(100);
+            
+            analyseSquare(current_pos, WEST, ping_cm(8));
+            turn(180);
+            pause(100);
+            
+            analyseSquare(current_pos, EAST, ping_cm(8));
+            pause(100);
+            
+            if (num_free_paths > 1) { 
+                current_pos->is_junction = true;
+                printf("Recorded current square as a junction\n");
+            }
+        }    
+        else {
+            if (!current_pos->is_junction) { // if square is part of a path, just keep going
+                printf("Continuing down previously visited path...\n");
+                direction_to_move = continuePath(current_pos);
+            }
+            else {
+                analyseSquare(current_pos, NORTH, 69);
+                analyseSquare(current_pos, WEST, 69);
+                analyseSquare(current_pos, EAST, 69);
+            }
+        }
+        
+        // turn to face appropriate square
+        switch (direction_to_move) {
+            case NORTH:
+                turn_amount = (turned) ? -90 : 0;
+                break;
+            case WEST:
+                turn_amount = (turned) ? -180 : -90;
+                break;
+            case SOUTH:
+                turn_amount = (turned) ? 90 : 180;
+                break;
+            case EAST:
+                turn_amount = (turned) ? 0 : 90;
+                break;
+        }
+        turn(turn_amount);
+        
+        if (current_pos->visited < TWICE) {
+            if (prev_square != NULL) {
+                if (prev_square->is_junction) {
+                    current_pos->visited++;
+                }
+                else {
+                    current_pos->visited = ONCE;
+                }
+            }
+            else current_pos->visited = ONCE;    
+        } 
+            
+        currentDirection = calculateCardinalDirection(direction_to_move);
+        prev_square = current_pos;
+        
+        switch (currentDirection) {
+            case NORTH:
+                current_pos = current_pos->north;
+                break;
+            case WEST:
+                current_pos = current_pos->west;
+                break;
+            case EAST:
+                current_pos = current_pos->east;
+                break;
+            case SOUTH:
+                current_pos = current_pos->south;
+                break;
+        }
+        
+        if (current_pos->is_junction && prev_square->visited < TWICE) prev_square->visited++;
+        lowest_visited_option = prev_square->visited;
+        
+        forwards(FWD);
     }
-  }
-    top_node = current_node;
-    
-    turnWithoutStore(179);
-    
-    while (current_node->next != NULL) {
-        drive_goto(current_node->right, current_node->left);
-        current_node = current_node->next;
-    }
-    freeStack(top_node);
+     
 }
