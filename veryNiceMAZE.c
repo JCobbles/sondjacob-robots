991fb9cc110d3a86acdfce4bfd81cec5be7bccd0
Jacob Moss <cob.mossy@gmail.com>
2017-03-24 10:51:17 +0000

    shorten

--- veryNiceMAZE.c
+++ veryNiceMAZE.c
@@ -111,7 +111,6 @@ int isRightDirection(Square* square) {
 
 //////////////////////////////////////////////
 
-
 #define MAX 16
 
 int intArray[MAX];
@@ -119,21 +118,13 @@ int front = 0;
 int rear = -1;
 int itemCount = 0;
 
-int peek() {
-   return intArray[front];
-}
-
 int isEmpty() {
    return itemCount == 0;
 }
 
 int isFull() {
    return itemCount == MAX;
-}
-
-int size() {
-   return itemCount;
-}  
+} 
 
 void insert(int data) {
 
